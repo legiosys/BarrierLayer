7a503155d06485b6b88f595bb91e9ae038c79aba
Alex Legio <legiosys@gmail.com>
2020-09-21 18:26:58 +0300

    Initial

--- BarrierLayer/Controllers/BarrierController.cs
+++ BarrierLayer/Controllers/BarrierController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BarrierLayer.Models;
+using Flurl;
+using Flurl.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +14,27 @@ namespace BarrierLayer
     [ApiController]
     public class BarrierController : ControllerBase
     {
+        string url = "https://api.privratnik.net:44590/app/api.php";
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            //await url.PostUrlEncodedAsync()
+            return Request.Path;
+        }
+        //key 1b92654f6afcd96f801eac91b0ea8b52
+
+        [HttpPost("number")]
+        public async Task<bool> SendSms(string number)
+        {
+            var response = await url.AppendPathSegment("sendSms").PostUrlEncodedAsync(new { number = number });
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        [HttpPost("checkCode")]
+        public async Task<StateResponse> CheckCode(string number, string code)
+        {
+            var response = await url.AppendPathSegment("checkCode").PostUrlEncodedAsync(new { number = number, smsCode = code }).ReceiveJson<StateResponse>();
+            return response;
+        }
     }
 }
---
+++ BarrierLayer/Models/StateResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarrierLayer.Models
+{
+    public class StateResponse
+    {
+        public string Key { get; set; }
+        public string Login { get; set; }
+        public int State { get; set; }
+    }
+}
