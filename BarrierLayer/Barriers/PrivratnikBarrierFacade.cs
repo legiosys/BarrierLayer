5e159861511ce76fbe6e3eaaa711fb617a4601a5
Alex Legio <legiosys@gmail.com>
2024-11-21 21:34:03 +0300

    New api

--- BarrierLayer/Barriers/PrivratnikBarrierFacade.cs
+++ BarrierLayer/Barriers/PrivratnikBarrierFacade.cs
@@ -1,11 +1,8 @@
-﻿using BarrierLayer.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Flurl;
-using Flurl.Http;
+﻿using System;
 using System.Threading.Tasks;
 using BarrierLayer.Models;
+using BarrierLayer.Services;
+using Flurl.Http;
 using Newtonsoft.Json;
 
 namespace BarrierLayer.Barriers
@@ -17,40 +14,44 @@ namespace BarrierLayer.Barriers
 
         public PrivratnikBarrierFacade(ConfigService config, Barrier barrier)
         {
-            _url = config.GetValue("PrivratnikUrl", "url").Result; 
+            _url = config.GetValue("PrivratnikUrl", "url").Result;
             _barrier = barrier;
-
         }
 
         public async Task<BarrierResponse> Confirm(string userNumber, string code)
         {
-            var response = await _url.AppendPathSegment("checkCode").PostUrlEncodedAsync(new { number = userNumber, smsCode = code }).ReceiveJson<StateResponse>();
-            return response.ToBarrierResponse();
+            var response = await _url
+                .PostMultipartAsync(mp => mp
+                    .AddString("number", userNumber)
+                    .AddString("smsCode", code))
+                .ReceiveJson<ConfirmResponse>();
+            return new BarrierResponse()
+            {
+                Key = response.Key
+            };
         }
 
         private async Task<BarrierResponse> OpenWithRefused()
         {
-            var response = await _url.AppendPathSegment("openAddedBarrier")
-                        .PostUrlEncodedAsync(
-                        new
-                        {
-                            login = _barrier.UserNumber,
-                            key = _barrier.Token,
-                            from = _barrier.UserNumber,
-                            to = _barrier.BarrierNumber
-                        });
+            var response = await _url
+                .PostMultipartAsync(mp => mp
+                    .AddString("barrier_id", _barrier.BarrierNumber)
+                    .AddString("command", "open")
+                    .AddString("key", _barrier.Token)
+                    .AddString("login", _barrier.UserNumber));
             var responseString = await response.GetStringAsync();
             Console.WriteLine($"Open result {response.StatusCode}: {responseString}");
             var responseJson = JsonConvert.DeserializeObject<StateResponse>(responseString);
             return responseJson.ToBarrierResponse();
         }
+
         public async Task<BarrierResponse> Open()
         {
             try
             {
                 return await OpenWithRefused();
             }
-            catch(FlurlHttpException ex)
+            catch (FlurlHttpException ex)
             {
                 if (ex.Message.Contains("Connection refused"))
                 {
@@ -66,25 +67,26 @@ namespace BarrierLayer.Barriers
 
         public async Task<BarrierResponse> Register(string userNumber)
         {
-            var response = await _url.AppendPathSegment("sendSms").PostUrlEncodedAsync(new { number = userNumber }).ReceiveJson<StateResponse>();
+            var response = await _url
+                .PostMultipartAsync(mp => mp
+                    .AddString("number", userNumber))
+                .ReceiveJson<StateResponse>();
             return response.ToBarrierResponse();
         }
 
         public class StateResponse
         {
-            public string Key { get; set; }
-            public string Login { get; set; }
             public int State { get; set; }
 
             public BarrierResponse ToBarrierResponse()
             {
                 return new BarrierResponse()
                 {
-                    Key = this.Key,
-                    Login = this.Login,
                     State = this.State
                 };
             }
         }
+
+        public record ConfirmResponse(string Key);
     }
 }
